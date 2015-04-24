cb7ca753d550737446a2b09d5d2a1e96730f4d67
Jeff Wu <jeffersonwu@Jeffersons-MacBook-Air.local>
2015-04-24 16:21:16 -0700

    adding AudioDirector, Raycast_Laser, and Raycast_Shooter

---
+++ Raycast_Laser.cs
@@ -0,0 +1,52 @@
+﻿//==================================
+// 		RAYCAST POINTER
+//==================================
+/*
+	Version 0.1 GIT, 2015.APR.23:
+	- uses right mouse button press for activation, orients to the surface normal of the object.
+ */
+
+
+using UnityEngine;
+using System.Collections;
+
+public class Raycast_Laser : MonoBehaviour {
+
+	//laser dot
+	public GameObject laserDot;
+
+	//seperate target object as a location orienter (experimental)
+	public GameObject target;
+
+	private RaycastHit myHit;
+
+	public float raycastDist = 100f;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		//press RMB to cast the object
+		if (Input.GetButton ("Fire2")) {
+
+			if (Physics.Raycast(transform.position, transform.forward, out myHit, raycastDist)) {
+				print("Yes, I hit something hit-able by the name of " + myHit.collider.gameObject.name);
+
+				laserDot.transform.position = myHit.point;
+				//laserDot.transform.rotation = Quaternion.LookRotation(myHit.normal);
+				laserDot.transform.rotation = Quaternion.LookRotation(myHit.normal);
+
+				//debug rotation of target object to see if it's moving
+				Debug.Log (target.transform.position);
+			}
+
+
+		}
+
+		else { 
+			//returns to world origin
+			laserDot.transform.position = new Vector3(0,0,0);
+		}
+	}
+
+	//CUSTOM FUNCTIONS
+}
---
+++ Raycast_Shooter.cs
@@ -0,0 +1,64 @@
+﻿/*======================
+ * Raycast Shooter v0.1
+ * =====================
+ * Initial GIT commit 2015.APR.23
+
+
+
+
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class Raycast_Shooter : MonoBehaviour {
+	
+	Color myColor = new Color(0.0f, 0.5f, 0.0f);
+	float rayLength = 25.0f;
+	
+	RaycastHit myHit;	//reference to the hit object 
+
+	// Update is called once per frame
+	void Update () {
+		
+		GameObject whatWasHit;	//object hit
+		
+		Vector3 myForward = transform.TransformDirection(Vector3.forward) * rayLength;	//10 units forward
+		Debug.DrawRay(transform.position, myForward, myColor);	//draw debug ray
+
+		//click button raycast
+		if (Input.GetButtonDown("Fire1")) {
+
+			if (Physics.Raycast(transform.position, myForward, out myHit, rayLength)) {
+
+				//debug lines of info
+				Debug.Log ("I've hit " + myHit.collider.gameObject);
+				whatWasHit = myHit.collider.gameObject;
+				Debug.Log ("What was hit: " + whatWasHit);
+
+				//find the enemy object that was hit
+				GameObject enemy = myHit.collider.gameObject;
+
+				//find damage script
+				ColorChanger eh = enemy.GetComponent<ColorChanger>();
+
+				//decrement health
+				if (eh.health >= 0) {
+					eh.health -= 10;
+
+					//if health is less than zero, reset to zero.
+					if (eh.health < 0) {
+						eh.health = 0;
+					}
+				}
+
+
+
+			}
+		}
+
+
+		
+		
+	}
+}
